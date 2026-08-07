db524ed40b5bf4dda569e0a9888023eac8fc6050
Mohamad82a <mohamad2003abasi@gmail.com>
2026-08-07 21:40:00 +0330

    Feat(P01,P02,P03): add examples to schemas

--- part1_rabbitmq_fundamentals/p01_basic_queue/app/api/schemas.py
+++ part1_rabbitmq_fundamentals/p01_basic_queue/app/api/schemas.py
@@ -1,7 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Dict, Any
 
 
 class Task(BaseModel):
-    name: str
-    body: Dict[str, Any]
+    name: str = Field(
+        ...,
+        examples=['generate_report']
+    )
+    body: Dict[str, Any] = Field(
+        ...,
+        examples=[
+            {
+                'format': 'pdf',
+                'user_id': 'user_123',
+                'department': 'finance'
+            }
+        ]
+    )
